ba33879f615df2192a887f9cbf459b22e255e07d
scam_wilson <sc_wilson@outlook.com>
2020-01-16 09:01:54 +0000

    training

---
+++ Tutorial/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//JSX
+ReactDOM.render(
+  <ul>
+    <li>Buy Juice</li>
+    <li>Walk Dog</li>
+    <li>Laith</li>
+  </ul>,
+  document.getElementById("root")
+);
