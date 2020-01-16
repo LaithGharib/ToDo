ed6bceb7188737f606291d8070d60a0f1778f92f
scam_wilson <sc_wilson@outlook.com>
2020-01-16 15:33:49 +0000

    new

---
+++ Tutorial/src/Components/MainContent.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function MainContent() {
+  return (
+    <div>
+      <h1>
+        <u>Things I hate</u>
+      </h1>
+      <ui>
+        <li>Peas</li>
+      </ui>
+    </div>
+  );
+}
+
+export default MainContent;
