f0394f41da9bd8975473033edb1b90ec308e34a3
scam_wilson <sc_wilson@outlook.com>
2020-01-18 19:09:46 +0000

    features added

--- Tutorial/src/Components/App.js
+++ Tutorial/src/Components/App.js
@@ -1,20 +1,30 @@
 import React from "react";
-
-import Header from "./Header";
-import MainContent from "./MainContent";
-import Footer from "./Footer";
-import Name from "./Name";
+import ReactDOM from "react-dom";
 
 function App() {
+  const date = new Date();
+  const hours = date.getHours();
+  let timeOfDay;
+  //inline styles
+  const styles = {
+    fontSize: 200,
+    backgroundColor: "#66ccff"
+  };
+
+  if (hours < 12) {
+    timeOfDay = "morning";
+    styles.color = "#G4756F";
+  } else if (hours < 18) {
+    timeOfDay = "afternoon";
+    styles.color = "#2E0927";
+  } else {
+    timeOfDay = "evening";
+    styles.color = "#D90000";
+  }
+
   return (
     <div>
-      <Header />
-      <Name />
-      <MainContent />
-      <MainContent />
-      <MainContent />
-      <MainContent />
-      <Footer />
+      <h1 style={styles}>Good {timeOfDay} Sir</h1>
     </div>
   );
 }
